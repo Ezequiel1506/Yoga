88fa818a18bf5734dac56022df7d82047e07da7f
Ezequiel Altamirano <altamirano.ezequielsergio@gmail.com>
2025-03-16 17:23:53 -0300

    new videos

---
+++ videos.js
@@ -0,0 +1,26 @@
+const videos = [
+    {
+        title: "Tadasana",
+        category: "Posturas de Pie",
+        videoID: "1WHfqJg6rq-cyVSKtCiJP9hs9n5-rRwhB",
+        description: "Postura de montaña, ideal para alinear el cuerpo."
+    },
+    {
+        title: "Virabhadrasana",
+        category: "Posturas de Pie",
+        videoID: "1jS7krtcVYoUOvZMslBBt-aGrrnNqJudN",
+        description: "Postura del guerrero, fortalece piernas y brazos."
+    },
+    {
+        title: "Padmasana",
+        category: "Posturas Sentadas",
+        videoID: "1DntOc-K90MjVpoErcG0XOXyJvgqF8X78",
+        description: "Postura de loto, ideal para meditar."
+    },
+    {
+        title: "Sukhasana",
+        category: "Posturas Sentadas",
+        videoID: "1pzfsW_q7TZZEDSXyyLFlqgsXtQUpDUNP",
+        description: "Postura fácil de sentarse, ideal para la meditación."
+    }
+];
